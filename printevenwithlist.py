d940c95c16cc0e908cc2060d50f72e2aef73dba8
yaminikanthch <yaminikanthch@gmail.com>
2017-05-11 18:41:44 +0530

    new examples on even and odd

---
+++ printevenwithlist.py
@@ -0,0 +1,9 @@
+# Step1 - Collect all the numbers in list
+a=[1,2,3,4,5,6,7,8,9,10]
+
+#Step2 - Wirte some logic to get all even numbers
+for i in a:
+	if i % 2 == 0:
+
+#Step3 - Print even numbers to screen
+		print i
---
+++ printoddwithlist.py
@@ -0,0 +1,8 @@
+#step1 get all the numbers in the list
+a = [1,2,3,4,5,6,7,8,9,10]
+
+#step2 - Write some logic  to print odd numbers
+for i in a:
+	if i % 2 != 0:
+#Step3 - Print to the screen
+		print i
